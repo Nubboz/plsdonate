61e8ac999207d15557185fc20a094a52cba620c7
Nubboz <114282336+Nubboz@users.noreply.github.com>
2022-09-24 10:10:50 +0100

    Update plsdonatescript.lua

--- plsdonatescript.lua
+++ plsdonatescript.lua
@@ -43,7 +43,7 @@ local queueonteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or 
 local httprequest = (syn and syn.request) or http and http.request or http_request or (fluxus and fluxus.request) or request
 local httpservice = game:GetService('HttpService')
 queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/tzechco/roblox-scripts/main/PLS%20DONATE/autofarm.lua'))()")
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tzechco/roblox-scripts/main/UI/ui-engine-v2.lua"))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nubboz/plsdonate/main/UIengine.lua"))()
 
 local function claimGifts()
     pcall(function()
